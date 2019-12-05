0a07c8cf59265848ce351505440d9c3c890ff4b2
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2019-12-05 15:22:12 +0900

    Add return statement

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"sample-interpreter/ast"
 	"sample-interpreter/lexer"
 	"sample-interpreter/token"
@@ -8,18 +9,30 @@ import (
 
 type Parser struct{
 	l *lexer.Lexer
-
+	errors []string
 	curToken token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser{
-	p := &Parser{l:l}
+	p := &Parser{
+		l:l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string{
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType){
+	msg := fmt.Sprintf("expected next token to be %s. got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken(){
@@ -31,7 +44,7 @@ func (p *Parser) ParseProgram() *ast.Program{
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.curToken.Type != token.EOF{
+	for !p.curTokenIs(token.EOF){
 		stmt := p.parseStatement()
 		if stmt != nil{
 			program.Statements = append(program.Statements, stmt)
@@ -46,6 +59,8 @@ func (p *Parser) parseStatement() ast.Statement{
 	switch p.curToken.Type{
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -83,5 +98,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool{
 		p.nextToken()
 		return true
 	}
+	p.peekError(t)
 	return false
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.SEMICOLON){
+		p.nextToken()
+	}
+	return stmt
 }
