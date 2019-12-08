0f823f352e968fda2ac7f77b2c5966cc50b5bf24
Tommy <ringkock@gmail.com>
2019-12-08 15:52:48 +0900

    Update parser. parsed function

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"sample-interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -198,5 +199,28 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+	return out.String()
+}
+
+type FunctionalLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionalLiteral) expressionNode()      {}
+func (fl *FunctionalLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionalLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 	return out.String()
 }
